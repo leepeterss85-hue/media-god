2b5cebfb7e4b5aac071643afdbbf3316b2092ef6
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-02 20:21:14 +0100

    Update QueryClientProvider import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
-import { QueryClientProvider } from '@tanstack/query-client'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
