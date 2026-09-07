a34b99c08b0b48dd03e3d890f0db6de198132734
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-07 08:51:31 +0100

    Remember Fire TV focus across overlays

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
+import FireTvFocusMemory from '@/components/mg/FireTvFocusMemory.jsx'
 import '@/index.css'
 
 const userAgent =
@@ -218,5 +219,8 @@ if (isFireTv && typeof document !== 'undefined') {
 installFireTvBackHandler()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <>
+    <FireTvFocusMemory />
+    <App />
+  </>
 )
