b19e91c236bb591be65e5d42743e0a742c24c4e8
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-08 22:07:16 +0100

    Update freeTvPlaylist.js

--- src/components/mg/freeTvPlaylist.js
+++ src/components/mg/freeTvPlaylist.js
@@ -62,6 +62,8 @@ export const FREE_TV_PLAYLIST_URL =
 
 export const LIVE_TV_REGION = "GB";
 
+export const PUBLIC_DIRECT_CHANNELS = [];
+
 export const SKY_STREAM_OVERRIDES = {
   "sky sports main event": "https://live20.bozztv.com/trn03/gin-skysportsmainevent/index.m3u8",
   "sky sports premier league": "https://live20.bozztv.com/trn03/gin-skysportspl/index.m3u8",
