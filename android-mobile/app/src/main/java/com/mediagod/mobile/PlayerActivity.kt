ce15e5f250301c21670967019d6ba1a61c87bc38
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-15 12:03:08 +0100

    Fix Android media headset key constant

--- android-mobile/app/src/main/java/com/mediagod/mobile/PlayerActivity.kt
+++ android-mobile/app/src/main/java/com/mediagod/mobile/PlayerActivity.kt
@@ -178,7 +178,7 @@ class PlayerActivity : Activity() {
                 }
 
                 KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE,
-                KeyEvent.KEYCODE_HEADSEHOOK -> {
+                KeyEvent.KEYCODE_HEADSETHOOK -> {
                     activePlayer?.let {
                         if (it.isPlaying) it.pause() else it.play()
                         showControllerTemporarily()
