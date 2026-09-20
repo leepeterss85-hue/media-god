208e7ff590a1eb99f156017485ade911bc36f3da
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-20 10:31:36 +0100

    Release Fire TV 1.4.48 audio rescue retention

--- firetv-android/app/build.gradle.kts
+++ firetv-android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mediagod.firetv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 52
-        versionName = "1.4.47"
+        versionCode = 53
+        versionName = "1.4.48"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
