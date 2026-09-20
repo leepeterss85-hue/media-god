9c9d52affbda0b8c5d1f483335bef899708004a1
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-20 13:36:22 +0100

    Release Android mobile 1.0.36 native English audio override

--- android-mobile/app/build.gradle.kts
+++ android-mobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mediagod.mobile"
         minSdk = 23
         targetSdk = 35
-        versionCode = 36
-        versionName = "1.0.35"
+        versionCode = 37
+        versionName = "1.0.36"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
