6fc77d40e7c301b5204cbc110825823eefbe57cb
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-20 18:02:10 +0100

    Bump Fire TV to 1.4.55

--- firetv-android/app/build.gradle.kts
+++ firetv-android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mediagod.firetv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 59
-        versionName = "1.4.54"
+        versionCode = 60
+        versionName = "1.4.55"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
