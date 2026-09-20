ea933918b20af985c1bed99649ff6a013bfd371a
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-20 13:11:06 +0100

    Release Fire TV 1.4.49 English audio lock

--- firetv-android/app/build.gradle.kts
+++ firetv-android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mediagod.firetv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 53
-        versionName = "1.4.48"
+        versionCode = 54
+        versionName = "1.4.49"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
