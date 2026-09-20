248d7babd211a155b1e4b9bd95c61306f71ad896
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-20 13:11:03 +0100

    Release Android mobile 1.0.35 English audio lock

--- android-mobile/app/build.gradle.kts
+++ android-mobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mediagod.mobile"
         minSdk = 23
         targetSdk = 35
-        versionCode = 35
-        versionName = "1.0.34"
+        versionCode = 36
+        versionName = "1.0.35"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
