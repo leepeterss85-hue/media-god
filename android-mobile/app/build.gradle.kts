11335ad37391dbd0f7e2a15df1db1e86021ac31d
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-20 16:01:04 +0100

    Bump Android mobile to 1.0.39

--- android-mobile/app/build.gradle.kts
+++ android-mobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mediagod.mobile"
         minSdk = 23
         targetSdk = 35
-        versionCode = 39
-        versionName = "1.0.38"
+        versionCode = 40
+        versionName = "1.0.39"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
