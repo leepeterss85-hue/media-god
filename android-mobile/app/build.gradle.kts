ab22c58017fc2834b011ee1b25b38e9926e9200f
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-20 14:31:41 +0100

    Release Media God Mobile 1.0.38

--- android-mobile/app/build.gradle.kts
+++ android-mobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mediagod.mobile"
         minSdk = 23
         targetSdk = 35
-        versionCode = 38
-        versionName = "1.0.37"
+        versionCode = 39
+        versionName = "1.0.38"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
