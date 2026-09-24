de8a030a116f8456dbd937df280ec08c0bea1c8d
leepeterss85-hue <leepeterss85@gmail.com>
2026-09-24 13:25:07 +0100

    Fix Android mobile updates for code 54 installations (#36)

--- android-mobile/app/build.gradle.kts
+++ android-mobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mediagod.mobile"
         minSdk = 23
         targetSdk = 35
-        versionCode = 54
-        versionName = "1.0.53"
+        versionCode = 55
+        versionName = "1.0.54"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
